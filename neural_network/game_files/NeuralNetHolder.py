cf769e6f70c471b3a7c7cb52441a3d700eae1cdb
superchroma <59047297+superchroma@users.noreply.github.com>
2022-08-04 14:55:29 +0100

    Add files via upload

---
+++ neural_network/game_files/NeuralNetHolder.py
@@ -0,0 +1,10 @@
+
+class NeuralNetHolder:
+
+    def __init__(self):
+        super().__init__()
+
+    
+    def predict(self, input_row):
+        # WRITE CODE TO PROCESS INPUT ROW AND PREDICT X_Velocity and Y_Velocity
+        pass # this pass can be removed once you add some code
